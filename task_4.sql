9268c59da2069634c205b7ae202abef7e9e39a4a
Jonathan-T99 <jonathantwizere@gmail.com>
2025-10-16 21:49:35 +0100

    Discovering Databases

--- task_4.sql
+++ task_4.sql
@@ -2,15 +2,14 @@
 -- Print the full description of the table 'books' from the database 'alx_book_store'
 
 
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
+SELECT TABLE_NAME,
+COLUMN_NAME,
+DATA_TYPE,
+IS_NULLABLE,
+COLUMN_DEFAULT,
+COLUMN_KEY,
+EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
